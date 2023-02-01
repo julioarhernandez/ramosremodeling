0e24098741140408e02a81baa2b0d2c0f5473a33
Julio Rodriguez <juliorodriguez@ncl.com>
2023-02-01 17:32:19 -0500

    UI fixes

--- js/center.js
+++ js/center.js
@@ -1,8 +1,10 @@
 
 
 $(window).resize(function(){
-	$('.slogan-text').css({
-		position:'absolute',
-		top: ($(window).height() - $('.slogan-text').outerHeight())/2
-	});
+	if ($(window).width() > 768){
+		$('.slogan-text').css({
+			position:'absolute',
+			top: ($(window).height() - $('.slogan-text').outerHeight())/2
+		});
+	}
 });
